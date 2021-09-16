2b41468b07f17101083080805a2e806828e2244f
Zhe Wang <wangzhetju@gmail.com>
2021-09-16 09:43:43 +0100

    add circleci

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ -d "./.deploy_git" ]; then
+  echo "Removing .deploy_git folder..."
+  rm -rf ./.deploy_git
+  echo "Folder removed."
+fi
+
+# Initialize target with currently deployed files
+git clone --depth 1 --branch=master https://github.com/wangzhe3224/wangzhe3224.github.io.git .deploy_git
+
+cd .deploy_git
+
+# Remove all files before they get copied from ../public/
+# so git can track files that were removed in the last commit
+find . -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
+
+cd ../
+
+if [ ! -d "./public" ]; then
+  hexo generate
+fi
+
+# Run deployment
+hexo deploy
