0a7e47d97ad37b99646cf188510b0e8b63b66237
Zhe Wang <wangzhetju@gmail.com>
2022-12-20 23:02:49 +0000

    fix ci

--- themes/next/scripts/events/index.js
+++ themes/next/scripts/events/index.js
@@ -11,10 +11,12 @@ hexo.extend.filter.register('before_generate', () => {
   require('./lib/injects')(hexo);
   // Highlight
   require('./lib/highlight')(hexo);
+  // Menu and sub menu
+  require('./lib/navigation')(hexo);
 }, 0);
 
 hexo.on('ready', () => {
-  if (process.argv.includes('--next-disable-banner')) return;
+  if (!/^(g|s)/.test(hexo.env.cmd) || process.argv.includes('--next-disable-banner')) return;
   const { version } = require('../../package.json');
   hexo.log.info(`==================================
   ███╗   ██╗███████╗██╗  ██╗████████╗
